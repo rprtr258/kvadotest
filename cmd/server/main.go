8b900c38c1b5215fe6e09c7041c26391eb248071
Poletansky Viktor <poletansky@gmail.com>
2022-05-16 12:43:31 +0300

    Separate repo files further

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,17 +3,15 @@ package main
 
 import (
 	"context"
-	"database/sql"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	pb "github.com/rprtr258/kvadotest/api"
+	"github.com/rprtr258/kvadotest/internal/repositories"
 	"google.golang.org/grpc"
 )
 
@@ -21,109 +19,14 @@ var (
 	port = flag.Int("port", 50051, "The server port")
 )
 
-type Book struct {
-	Title   string
-	Content string
-	Authors []string
-}
-
-type BooksRepository interface {
-	SearchByAuthor(context.Context, string) ([]Book, error)
-	SearchByTitle(context.Context, string) ([]Book, error)
-	SearchByContent(context.Context, string) ([]Book, error)
-}
-
-type MysqlBooksRepository struct {
-	db *sql.DB
-}
-
-func readBookRows(rows *sql.Rows) ([]Book, error) {
-	res := make([]Book, 0)
-	var (
-		authors     []byte
-		bookTitle   string
-		bookContent string
-		authorsList []string
-	)
-	for rows.Next() {
-		rows.Scan(&authors, &bookTitle, &bookContent)
-		if err := json.Unmarshal(authors, &authorsList); err != nil {
-			return nil, err
-		}
-		res = append(res, Book{
-			Authors: authorsList,
-			Title:   bookTitle,
-			Content: bookContent,
-		})
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return res, nil
-}
-
-func (repo MysqlBooksRepository) SearchByAuthor(ctx context.Context, req string) ([]Book, error) {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-	rows, err = repo.db.QueryContext(ctx, `
-		WITH authors AS (
-			SELECT book_id
-			FROM book_list
-			WHERE author_name LIKE CONCAT('%', ?, '%')
-		)
-		SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
-		FROM book_list
-		GROUP BY book_id
-		HAVING book_id IN (select * from authors);
-	`, req)
-	if err != nil {
-		return nil, err
-	}
-	return readBookRows(rows)
-}
-func (repo MysqlBooksRepository) SearchByTitle(ctx context.Context, req string) ([]Book, error) {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-	rows, err = repo.db.QueryContext(ctx, `
-			SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
-			FROM book_list
-			WHERE book_title LIKE CONCAT('%', ?, '%')
-			GROUP BY book_id;
-		`, req)
-	if err != nil {
-		return nil, err
-	}
-	return readBookRows(rows)
-}
-func (repo MysqlBooksRepository) SearchByContent(ctx context.Context, req string) ([]Book, error) {
-	var (
-		rows *sql.Rows
-		err  error
-	)
-	rows, err = repo.db.QueryContext(ctx, `
-			SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
-			FROM book_list
-			WHERE book_content LIKE CONCAT('%', ?, '%')
-			GROUP BY book_id;
-		`, req)
-	if err != nil {
-		return nil, err
-	}
-	return readBookRows(rows)
-}
-
 type server struct {
 	pb.UnimplementedBookSearchServer
-	booksRepo BooksRepository
+	booksRepo repositories.BooksRepository
 }
 
 func (s *server) Search(ctx context.Context, in *pb.SearchRequest) (*pb.SearchReply, error) {
 	var (
-		res []Book
+		res []repositories.Book
 		err error
 	)
 	switch req := in.Request.(type) {
@@ -158,19 +61,14 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	db, err := sql.Open("mysql", "root:pass@/books")
+	booksRepo, err := repositories.NewMysqlRepository("root:pass@/books")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to connect to database: %v", err)
 	}
-	defer db.Close()
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-	ctx := context.Background()
-	if err = db.PingContext(ctx); err != nil {
-		log.Fatal(err)
+	defer booksRepo.Close()
+	srv := server{
+		booksRepo: booksRepo,
 	}
-	srv := server{booksRepo: MysqlBooksRepository{db}}
 	pb.RegisterBookSearchServer(s, &srv)
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
---
+++ internal/repositories/repo.go
@@ -0,0 +1,125 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"time"
+)
+
+type Book struct {
+	Title   string
+	Content string
+	Authors []string
+}
+
+type BooksRepository interface {
+	SearchByAuthor(context.Context, string) ([]Book, error)
+	SearchByTitle(context.Context, string) ([]Book, error)
+	SearchByContent(context.Context, string) ([]Book, error)
+	Close() error
+}
+
+type MysqlBooksRepository struct {
+	db *sql.DB
+}
+
+func NewMysqlRepository(dataSourceName string) (*MysqlBooksRepository, error) {
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	ctx := context.Background()
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+	return &MysqlBooksRepository{
+		db: db,
+	}, nil
+}
+
+func readBookRows(rows *sql.Rows) ([]Book, error) {
+	res := make([]Book, 0)
+	var (
+		authors     []byte
+		bookTitle   string
+		bookContent string
+		authorsList []string
+	)
+	for rows.Next() {
+		rows.Scan(&authors, &bookTitle, &bookContent)
+		if err := json.Unmarshal(authors, &authorsList); err != nil {
+			return nil, err
+		}
+		res = append(res, Book{
+			Authors: authorsList,
+			Title:   bookTitle,
+			Content: bookContent,
+		})
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (repo MysqlBooksRepository) SearchByAuthor(ctx context.Context, req string) ([]Book, error) {
+	var (
+		rows *sql.Rows
+		err  error
+	)
+	rows, err = repo.db.QueryContext(ctx, `
+		WITH authors AS (
+			SELECT book_id
+			FROM book_list
+			WHERE author_name LIKE CONCAT('%', ?, '%')
+		)
+		SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
+		FROM book_list
+		GROUP BY book_id
+		HAVING book_id IN (select * from authors);
+	`, req)
+	if err != nil {
+		return nil, err
+	}
+	return readBookRows(rows)
+}
+func (repo MysqlBooksRepository) SearchByTitle(ctx context.Context, req string) ([]Book, error) {
+	var (
+		rows *sql.Rows
+		err  error
+	)
+	rows, err = repo.db.QueryContext(ctx, `
+			SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
+			FROM book_list
+			WHERE book_title LIKE CONCAT('%', ?, '%')
+			GROUP BY book_id;
+		`, req)
+	if err != nil {
+		return nil, err
+	}
+	return readBookRows(rows)
+}
+func (repo MysqlBooksRepository) SearchByContent(ctx context.Context, req string) ([]Book, error) {
+	var (
+		rows *sql.Rows
+		err  error
+	)
+	rows, err = repo.db.QueryContext(ctx, `
+			SELECT JSON_ARRAYAGG(author_name) AS authors, book_title, book_content
+			FROM book_list
+			WHERE book_content LIKE CONCAT('%', ?, '%')
+			GROUP BY book_id;
+		`, req)
+	if err != nil {
+		return nil, err
+	}
+	return readBookRows(rows)
+}
+
+func (repo MysqlBooksRepository) Close() error {
+	return repo.db.Close()
+}
